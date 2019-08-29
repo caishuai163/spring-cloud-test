7acfca46d1c480b0ff3d749e9d9a03ef0d749446
caishuai <caishuai@gyyx.cn>
2019-08-29 18:01:55 +0800

    a

--- springtest/testspring-eureka-server/testspring-eureka-turbine/src/main/java/test/DashboardApplication.java
+++ springtest/testspring-eureka-server/testspring-eureka-turbine/src/main/java/test/DashboardApplication.java
@@ -2,12 +2,14 @@ package test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableHystrixDashboard
 @EnableTurbine
+@RefreshScope // 刷新配置，写在这里好像很多东西都可以重新加载，类似于重启了，但是端口是没有办法的  actuator/refresh 2.0以上都有actuator
 public class DashboardApplication {
     public static void main(String[] args) {
         SpringApplication.run(DashboardApplication.class, args);
